a15e48315ebd3f301b95e300f807b39b12195311
jinglingxing <jinlingbaby@icloud.com>
2020-05-10 22:49:57 -0400

    new changes

--- day7.py
+++ day7.py
@@ -1,51 +1,91 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri May  8 21:26:30 2020
+Created on Thu May  7 11:34:48 2020
 
 @author: jinlingxing
 """
-'''
-You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
 
- 
+'''
+In a binary tree, the root node is at depth 0, and children of each depth k node are at depth k+1.
+
+Two nodes of a binary tree are cousins if they have the same depth, but have different parents.
+
+We are given the root of a binary tree with unique values, and the values x and y of two different nodes in the tree.
+
+Return true if and only if the nodes corresponding to the values x and y are cousins.
 
  
 
 Example 1:
 
 
-
-Input: coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
-Output: true
+Input: root = [1,2,3,4], x = 4, y = 3
+Output: false
 Example 2:
 
 
+Input: root = [1,2,3,null,4,null,5], x = 5, y = 4
+Output: true
+Example 3:
 
-Input: coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+
+
+Input: root = [1,2,3,null,4], x = 2, y = 3
 Output: false
  
 
-Constraints:
+Note:
 
-2 <= coordinates.length <= 1000
-coordinates[i].length == 2
--10^4 <= coordinates[i][0], coordinates[i][1] <= 10^4
-coordinates contains no duplicate point.
-'''
-
+The number of nodes in the tree will be between 2 and 100.
+Each node has a unique integer value from 1 to 100.
+ '''
+ # Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
 class Solution:
-    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
-        if len(coordinates) == 2:
-            return True
-        elif len(coordinates) > 2:
-            for i in range(2, len(coordinates)):
-                if coordinates[1][0] - coordinates[0][0] != 0:
-                    a = ((coordinates[1][1] - coordinates[0][1])/(coordinates[1][0] - coordinates[0][0]))
-                    b = (coordinates[0][1]*coordinates[1][0] - coordinates[0][0]*coordinates[1][1])/(coordinates[1][0]-coordinates[0][0])
-                    if coordinates[i][1] == a*coordinates[i][0] + b:
-                        return True
-                    else:
-                        return False
-        else:  #len(coordinates) < 2
-            return False 
+    def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
+        depth = 0
+        parent = None
+        x_info = []
+        y_info = []
+        self.dfs(root, x, y, depth, parent, x_info, y_info)
+        return x_info[0][0] == y_info[0][0] and x_info[0][1]!=y_info[0][1]
+    def dfs(self, root, x, y, depth, parent, x_info, y_info):
+        if root is None:
+            return None
+        if root.val == x:
+            x_info.append((depth, parent))
+        if root.val == y:
+            y_info.append((depth, parent))
+        self.dfs(root.left, x, y, depth+1, root, x_info, y_info)
+        self.dfs(root.right, x, y, depth+1, root, x_info, y_info)
+        
+
+sol = Solution()  
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.right = TreeNode(4)
+sol.isSameDepth(root, 4)     
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
